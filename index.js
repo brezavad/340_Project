defa4b800c575aee6ff75efbe190c2335d052f57
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-05-09 19:37:00 -0400

    Added route for stores page.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main',
+});
 var request = require('request');
 
 app.engine('handlebars', handlebars.engine);
@@ -8,42 +10,42 @@ app.set('view engine', 'handlebars');
 app.set('port', 13331);
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/order', function(req, res) {
+app.get('/order', function (req, res) {
   res.render('order');
 });
 
-app.get('/customers', function(req, res) {
+app.get('/customers', function (req, res) {
   res.render('customers');
 });
 
-app.get('/allorders', function(req, res) {
+app.get('/allorders', function (req, res) {
   res.render('allorders');
 });
 
-app.get('/inventory', function(req, res) {
+app.get('/inventory', function (req, res) {
   res.render('inventory');
 });
 
-app.get('/stores', (req, res) => {
+app.get('/stores', function (req, res) {
   res.render('stores');
 });
 
-app.use(function(req,res){
+app.use(function (req, res) {
   res.status(404);
   res.render('404');
 });
 
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.type('plain/text');
   res.status(500);
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
   console.log('Listening on flip3.engr.oregonstate.edu: ' + app.get('port'));
 });
