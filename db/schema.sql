2f45e53bb7924be6c3ac848f0552ebc92a7d8b02
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-05-15 11:21:33 -0400

    Added some mock data.

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,12 @@ CREATE TABLE customers (
     email VARCHAR(256) NOT NULL
 );
 
+-- insert mock data into customers table
+INSERT INTO customers (first_name, last_name, email)
+VALUES('Rebecca', 'Johnson', 'rjonhson@gmail.com'),
+('John', 'Williams', 'johnwilliams@gmail.com'),
+('Emma', 'Wilson', 'ewilson@gmail.com');
+
 -- create stores table
 CREATE TABLE stores (
     store_id int AUTO_INCREMENT PRIMARY KEY,
@@ -14,6 +20,12 @@ CREATE TABLE stores (
     state CHAR(2) NOT NULL,
     zipcode CHAR(5) NOT NULL
 );
+
+-- create mock data into stores table
+INSERT INTO stores (street_address, city, state, zipcode)
+VALUES ('1 Broad Street', 'New York City', 'NY', '10065'),
+('26 Main Street', 'Providence', 'RI', '02860'),
+('5 Central Avenue', 'San Francisco', 'CA', '94016');
 
 -- create computer systems table
 CREATE TABLE computer_systems (
@@ -24,6 +36,12 @@ CREATE TABLE computer_systems (
     screen_size int NOT NULL
 );
 
+-- insert into mock data into computer_systems table
+INSERT INTO computer_systems (description, ram, hard_drive, screen_size)
+VALUES ('New computer designed for personal use.', 8, 128, 16),
+('Refurbished computer originally designed for office use.', 16, 256, 20),
+('Used computer designed for portability.', 4, 128, 12);
+
 -- create inventory table
 CREATE TABLE inventory (
     inventory_id int AUTO_INCREMENT PRIMARY KEY,
@@ -32,6 +50,12 @@ CREATE TABLE inventory (
     FOREIGN KEY(computer_id) REFERENCES computer_systems(computer_id),
     FOREIGN KEY(store_id) REFERENCES stores(store_id)
 );
+
+-- insert mock data into inventory table
+INSERT INTO inventory (computer_id, store_id)
+VALUES (3, 1),
+(1, 2),
+(2, 3);
 
 -- create orders table
 CREATE TABLE orders (
@@ -42,4 +66,10 @@ CREATE TABLE orders (
     FOREIGN KEY(customer_id) REFERENCES customers(customer_id),
     FOREIGN KEY(inventory_id) REFERENCES inventory(inventory_id)
 );
+
+-- insert mock data into orders table
+INSERT INTO orders (customer_id, inventory_id, quantity)
+VALUES (1, 3, 5),
+(2, 2, 3),
+(3, 1, 10);
 
