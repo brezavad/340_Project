f5317ef68594fc9a43d6de87ec42bf44f399265f
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-05-24 09:14:34 -0400

    Error in query.

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function getInventory(res, db, context, complete) {
 }
 
 function getOrders(res, db, context, complete) {
-  db.query(`SELECT o.order_id, cu.first_name, cu.last_name, co.id, o.quantity FROM orders o 
+  db.query(`SELECT o.order_id, cu.first_name, cu.last_name, co.computer_id, o.quantity FROM orders o 
 INNER JOIN customers cu ON o.order_id = cu.customer_id
 INNER JOIN inventory i ON i.inventory_id = o.order_id
 INNER JOIN computer_systems co ON i.inventory_id = co.computer_id;`,
